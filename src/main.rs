be6c361b7c92ae77becc221835b86ad1ae7d1153
Charlie <charliepapworth@protonmail.com>
2021-07-08 08:37:41 +0100

    Print progress to console (it'll do for now)

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,15 @@ fn main(){
                                     .unwrap();
 
   write!(file, "P3\n{} {} \n255\n", image_width, image_height);
-
+  let mut perc:i32 = 0;
 
     for j in 0..image_height{
+        // Loading Bar Output
+        if perc != ((j as f64)/(image_height as f64)*100.0) as i32 {
+            perc = ((j as f64)/(image_height as f64)*100.0) as i32;
+            println!("{}", ((j as f64)/(image_height as f64)*100.0) as i32);
+        }
+        // Write to Image
         for i in 0..image_width{
             let r = i as f64/(image_width as f64 - 1.0);
             let g =  (image_height - j) as f64/(image_height as f64 - 1.0);
