bccd5bca72eedba9a727c2729fbd025d06e747fe
Charlie <charliepapworth@protonmail.com>
2021-07-10 20:42:44 +0100

    Renamed vec3.rs to vec.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::fs::OpenOptions;
 use std::io::prelude::*;
 use std::io::{Error, Write};
-mod vec3;
-
+mod vec;
+mod ray;
 fn main(){
 
     let image_width = 256;
@@ -26,7 +26,7 @@ fn main(){
         }
         // Write to Image
         for i in 0..image_width{
-            let pixel_color = vec3::color::new(i as f64/(image_width as f64 - 1.0),
+            let pixel_color = vec::color::new(i as f64/(image_width as f64 - 1.0),
                                                     (image_height - j) as f64/(image_height as f64 - 1.0),
                                                     0.25);
             pixel_color.write_color(&mut file);
