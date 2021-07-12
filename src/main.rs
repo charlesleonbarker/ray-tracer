8721d6a5fbabce0fbe7ee764e016ce415f58b198
Charlie <charliepapworth@protonmail.com>
2021-07-12 12:19:42 +0100

    Added camera, viewport and ray-based rendering

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,32 @@ use std::io::{Error, Write};
 mod vec;
 use crate::vec::*;
 mod ray;
+use crate::ray::*;
+
+pub fn ray_color(r: Ray) -> Color{
+    let unit_dir = r.direction().unit_vector();
+    let t = 0.5*(unit_dir.y() + 1.0);
+    (1.0-t)*Color::new(1.0, 1.0, 1.0) + t*Color::new(0.5, 0.7, 1.0)
+}
+
 fn main(){
 
-    let image_width = 256;
-    let image_height = 256;
+    //Image
+    const aspect_ratio: f64 = 16.0/9.0;
+    const image_width:i32 = 256;
+    const image_height:i32 = ((image_width as f64)/aspect_ratio) as i32;
 
+    //Camera
+    let viewport_height = 2.0;
+    let viewport_width = aspect_ratio*viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Point3::new(0.0, 0.0,0.0 );
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner = origin - horizontal/2.0 - vertical/2.0 - Vec3::new(0.0, 0.0, focal_length);
+
+    //Render
     let path = "results.ppm";
     let mut file = OpenOptions::new()
                                     .create(true)
@@ -25,11 +46,11 @@ fn main(){
             perc = ((j as f64)/(image_height as f64)*100.0) as i32;
             println!("{}", ((j as f64)/(image_height as f64)*100.0) as i32);
         }
-        // Write to Image
         for i in 0..image_width{
-            let pixel_color = Color::new(i as f64/(image_width as f64 - 1.0),
-                                                    (image_height - j) as f64/(image_height as f64 - 1.0),
-                                                    0.25);
+            let u = i as f64/(image_width as f64 - 1.0);
+            let v = ((image_height - j) as f64)/((image_height - 1) as f64);
+            let r = Ray::new(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+            let pixel_color = ray_color(r);
             pixel_color.write_color(&mut file);
         }
     }
